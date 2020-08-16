28b8fefc9cb63bd68c68b1d899604c493f89e54e
GHYflysheep <gansughy@163.com>
2020-08-16 14:04:50 +0800

    newFile

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  port: 4000,
+  // magicNumber: 1.5,
+  magicNumber: 1.5,
+  device: 'android', // 配置目标设备，android为安卓设备，ios为苹果设备
+  url: 'http://169.254.254.66:8100' // ios设备WebDriverAgent地址
+}
